0d218bc8ea12cd638439389b6aa43c7a02cafa35
Ola <tundock007@gmail.com>
2017-07-08 17:03:13 -0700

    Updated functions

--- src/com/Olatunde/Main.java
+++ src/com/Olatunde/Main.java
@@ -19,4 +19,31 @@ public class Main {
         }
         return array;
     }
+
+    public static void printArray(int[] array) {
+        for(int i=0; i<array.length; i++) {
+            System.out.println("Element " + i + " contents " + array[i]);
+        }
+    }
+
+    public static int[] sortIntegers(int[] array) {
+        int[] sortedArray = new int[array.length];
+        for(int i=0; i<array.length; i++) {
+            sortedArray[i] = array[i];
+        }
+        boolean flag = true;
+        int temp;
+        while(flag) {
+            flag = false;
+            for(int i =0; i<sortedArray.length; i++) {
+                if(sortedArray[i] < sortedArray[i+1]) {
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i+1];
+                    sortedArray[i+1] = temp;
+                    flag = true
+                }
+            }
+        }
+        return sortedArray;
+    }
 }
